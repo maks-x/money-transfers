8280b78d0aab3df9d0ecb91c8e17cee1f3fe1692
maks-x <100487620+maks-x@users.noreply.github.com>
2022-03-20 11:17:34 +0300

    Upgrade fee system (add MasterCard, Maestro, Visa, Mir accounts)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,78 @@
-const val amount = 1_000_000
+const val VK_PAY = "VK Pay"
+const val MASTER_CARD = "MasterCard"
+const val MAESTRO = "Maestro"
+const val VISA = "Visa"
+const val MIR = "Мир"
+
+const val RUB = 100
+
+const val AMOUNT = 10_000 * RUB
 
 fun main() {
-    val fee = calcFee(amount)
-    println("""
-        Transfer amount: $amount kopecks.
-        The commission will be: $fee kopecks.
-    """.trimIndent())
+    moneyTransfer(amount = AMOUNT)
+    moneyTransfer(MASTER_CARD, AMOUNT, 66_000 * RUB)
+    moneyTransfer(MAESTRO, AMOUNT)
+    moneyTransfer(MAESTRO, 299 * RUB)
+    moneyTransfer(MIR, AMOUNT)
+    moneyTransfer(VISA, 500 * RUB)
+    moneyTransfer("some account", AMOUNT)
 }
 
-fun calcFee(amount: Int): Int{
-    val possibleFee = amount / 10_000 * 75
-    val minFee = 3_500
+fun moneyTransfer(accountType: String = VK_PAY, amount: Int, monthAmount: Int = 0) {
+    val fee = calcFee(accountType, amount, monthAmount)
+    when (fee) {
+        -1 -> println("\nAccount type: $accountType\nUnknown account type")
+        else -> {
+            println(
+                """
+        
+            Account type: $accountType
+            Month amount: ${kopecksToRubles(monthAmount)} RUB.
+            Transfer amount: ${kopecksToRubles(amount)} RUB.
+            The commission will be: ${kopecksToRubles(fee)} RUB.
+            """.trimIndent()
+            )
+        }
+    }
+}
+
+fun calcFee(
+    accountType: String = VK_PAY,
+    amount: Int,
+    monthAmount: Int = 0
+): Int {
+    return when (accountType) {
+        VK_PAY -> 0
+        MASTER_CARD -> masterCardFee(amount, monthAmount)
+        MAESTRO -> maestroFee(amount, monthAmount)
+        VISA -> visaFee(amount)
+        MIR -> mirFee(amount)
+        else -> -1
+    }
+}
+
+fun masterCardFee(amount: Int, monthAmount: Int): Int {
+    return when {
+        amount >= 300 * RUB && (monthAmount + amount) <= 75_000 * RUB -> 0
+        else -> (0.006 * amount + 20 * RUB).toInt()
+    }
+}
+
+fun mirFee(amount: Int): Int {
+    val possibleFee = (0.0075 * amount).toInt()
+    val minFee = 35 * RUB
     return if (possibleFee > minFee) possibleFee else minFee
 }
 
+//вдруг в будущем мы захотим поменять логику для Maestro и Visa
+fun maestroFee(amount: Int, monthAmount: Int): Int {
+    return masterCardFee(amount, monthAmount)
+}
+
+fun visaFee(amount: Int): Int {
+    return mirFee(amount)
+}
+
+fun kopecksToRubles(amount: Int): Double {
+    return amount.toDouble() / RUB
+}
